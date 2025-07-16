84c6255a6dbc46a49e58f8efa8ddbd990ac33733
Shruti Mahadik <shrutimahadik2102@gmail.com>
2025-07-16 09:58:11 +0530

    Fix: Correct backend API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL:'https://taskmaster-backend-foln.onrender.com/api/',
 });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('access');

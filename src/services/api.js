6dc63458e3d01b0a6d4493011edf61daf6ba2b1e
Shruti Mahadik <shrutimahadik2102@gmail.com>
2025-07-16 12:00:05 +0530

    api chnge

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
     baseURL:  process.env.REACT_APP_API_URL,
 });
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('access');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
